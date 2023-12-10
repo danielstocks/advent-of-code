1ab6b76f05c0860466c50abf3b6286ce4cd3d2a1
Daniel Stocks <daniel@webcloud.se>
2023-12-10 11:38:46 +0100

    part 5 done, but takes 5 minutes to run :D

--- javascript/day5.js
+++ javascript/day5.js
@@ -1,50 +1,64 @@
-function createRange(start, end) {
-    const range = [];
-    for (let i = start; i <= end; i++) {
-        range.push(i);
-    }
-    return range;
-}
 
 function createMaprange(input) {
 
     return input.split("\n").slice(1).map((row) => {
-
-        const text = row.split(" ").map((text) => parseInt(text, 10))
-        console.log(text)
-
-        return {
-            sources: createRange(text[1], text[1] + text[2] - 1),
-            destinations: createRange(text[0], text[0] + text[2] - 1)
-        }
+        const [source, destination, range] = row.split(" ").map((text) => parseInt(text, 10))
+        return [source, destination, range];
     })
+}
+
+function seekAndFind(start, end, find) {
+
+    var index = 0;
+
+    for (var s = start; s < end; s++) {
+        if (find == s) {
+            return index
+        }
+        index++
+    }
+    return -1
 }
 
 function getDestinationFromSource(sourceToFind, map) {
 
-    let matchRowIndex = -1;
-    let matchSourceIndex = -1;
+    let sourceIndexFound = -1
+    let rowIndexFound = - 1
 
-
-    map.forEach((row, rowIndex) => {
-        row.sources.forEach((source, sourceIndex) => {
-            if (sourceToFind === source) {
-                matchRowIndex = rowIndex
-                matchSourceIndex = sourceIndex
-            }
-        })
+    map.every((row, rowIndex) => {
+        sourceIndexFound = seekAndFind(row[1], row[1] + row[2], sourceToFind)
+        if (sourceIndexFound !== -1) {
+            rowIndexFound = rowIndex
+            return false;
+        } else {
+            return true;
+        }
     })
 
-    if (matchRowIndex == -1 || matchSourceIndex == -1) {
-        return sourceToFind;
-    }
+    if (rowIndexFound !== -1) {
 
-    return map[matchRowIndex].destinations[matchSourceIndex]
+        const row = map[rowIndexFound]
+        const start = row[0];
+        const end = row[0] + row[2]
+        let index = 0
+        let destinationValue;
+
+        for (var s = start; s < end; s++) {
+            if (sourceIndexFound == index) {
+                destinationValue = s;
+            }
+            index++
+        }
+
+        return destinationValue;
+
+    } else {
+        return sourceToFind
+    }
 
 }
 
 function getSeedLocation(maps, seed) {
-
     let soil = getDestinationFromSource(seed, maps["seed-soil"]);
     let fertilizer = getDestinationFromSource(soil, maps["soil-fertilizer"]);
     let water = getDestinationFromSource(fertilizer, maps["fertilizer-water"]);
@@ -52,30 +66,15 @@ function getSeedLocation(maps, seed) {
     let temperature = getDestinationFromSource(light, maps["light-temperature"]);
     let humidity = getDestinationFromSource(temperature, maps["temperature-humidity"]);
     let location = getDestinationFromSource(humidity, maps["humidity-location"]);
-
-    console.log(
-        "Seed", seed,
-        ", soil", soil,
-        ", fertilizer", fertilizer,
-        ", water", water,
-        ", light", light,
-        ", temperature", temperature,
-        ", humidity", humidity,
-        ", location", location
-    )
-
+    console.log("found location:", location)
     return location
 }
 
 
 export function run(input) {
 
-    
-
     const data = input.trim().split("\n\n");
-
     const seeds = data[0].split(" ").slice(1).map(seed => parseInt(seed, 10))
-
     const maps = {
         "seed-soil": createMaprange(data[1]),
         "soil-fertilizer": createMaprange(data[2]),
@@ -85,19 +84,5 @@ export function run(input) {
         "temperature-humidity": createMaprange(data[6]),
         "humidity-location": createMaprange(data[7])
     }
-
     return Math.min(...seeds.map(seed => getSeedLocation(maps, seed)))
 }
-
-/* 
-
-Thinking / Rationale
---------------------
-1. Extract seeds - Done
-2. Extract maps - Done 
-3. Fill in the full ranges in maps - Done
-4. Connect Seed to location through maps 
-   Seed => Soil => fertilizer => Water => Light => Temperature => Humidity => Location
-5. Get lowest location numbers
-
-*/
