1d7cc43cd5b92b0007aea1f33cd3efe76c25a8f0
Daniel Stocks <daniel@webcloud.se>
2023-12-12 07:42:57 +0100

    Improved performance of part 2 by using quadratic function to avoid iteration

--- javascript/day6-part2.js
+++ javascript/day6-part2.js
@@ -1,3 +1,26 @@
+function getWinningCases(time, distance) {
+
+    let delta = Math.sqrt(time * time - (4 * distance));
+
+    let minValue = (time - delta) / 2;
+    let maxValue = (time + delta) / 2;
+
+    if (minValue - Math.floor(minValue) === 0) {
+        minValue += 1;
+    } else {
+        minValue = Math.ceil(minValue);
+    }
+
+    if (maxValue - Math.floor(maxValue) === 0) {
+        maxValue -= 1;
+    } else {
+        maxValue = Math.floor(maxValue);
+    }
+
+    return Math.floor(maxValue - minValue) + 1;
+}
+
+
 export function run(input) {
 
     const [time, currentRecordDistance] = input
@@ -7,7 +30,5 @@ export function run(input) {
             row => parseFloat(row.split(":")[1])
         );
 
-    return [...Array(time).keys()].filter(speed => {
-        return (time - speed) * speed > currentRecordDistance
-    }).length
+    return getWinningCases(time, currentRecordDistance)
 }
