a59d9dbf28a1f8d25c310db8f9e4f6715d58a37a
Daniel Stocks <daniel@webcloud.se>
2023-12-23 18:18:52 +0100

    Day 8 part 2 done
    I was a bit naive again with sample data and tried to bruteforce it but quikly realsied the full input would need a more sofisticated solution
    
    Ended up collecting the numbers steps required for each node to reach Z. Instead of running all "path finiders" in paralell until they all end up at Z I use some mat to find the least common multiple of all numbers together. This allows me to figure the minimum steps required for all paths to simoultanesouly end up at Z

--- javascript/day8.js
+++ javascript/day8.js
@@ -3,7 +3,7 @@ export function run(input) {
     // Parse instructions and L = 0, R = 1 for easier array access later on.
     const instructions = input.trim().replaceAll("L", 0).replaceAll("R", 1).split("\n")[0]
 
-    // Parse and create node graph structure
+    // Parse and create nodes
     const nodes = {}
     input.trim().split("\n").slice(2).forEach(node => {
         const paths = node.replace(/[\s=(),]+/g, " ").trim().split(" ")
@@ -14,18 +14,18 @@ export function run(input) {
     let currentStep = 0;
     let found = false;
     let nextDestination = "AAA";
-    
-    while (found == false) {
+
+    while (!found) {
         totalSteps++;
         nextDestination = nodes[nextDestination][instructions[currentStep]]
         currentStep++;
         if (currentStep >= instructions.length) {
             currentStep = 0;
         }
-        if(nextDestination == "ZZZ") {
+        if (nextDestination == "ZZZ") {
             found = true;
         }
     }
     return totalSteps;
-    
+
 }
--- javascript/day8.test.js
+++ javascript/day8.test.js
@@ -1,5 +1,6 @@
 import { expect, test, describe } from "bun:test";
 import { run } from './day8'
+import { run as run2 } from './day8-part2'
 const file = Bun.file("../input/day8.txt");
 const input = await file.text();
 
@@ -23,8 +24,22 @@ BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)
 `
 
+const part2TestInput = `
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+`
+
 
 describe("day 8", () => {
+
     test("part 1 - test input", () => {
         expect(run(testInput)).toBe(2)
     });
@@ -36,4 +51,13 @@ describe("day 8", () => {
     test("part 1 - input", () => {
         expect(run(input)).toBe(19199)
     });
+
+    test("part 2 - test input", () => {
+        expect(run2(part2TestInput)).toBe(6)
+    });
+
+    test("part 2 - input", () => {
+        expect(run2(input)).toBe(13663968099527)
+    });
+
 })
