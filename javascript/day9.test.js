85e00c24777e6e5945b96fee55ca58209e2e9fae
Daniel Stocks <daniel@webcloud.se>
2023-12-25 00:07:52 +0100

    day 9 part 2 done

--- javascript/day9.test.js
+++ javascript/day9.test.js
@@ -1,5 +1,6 @@
 import { expect, test, describe } from "bun:test";
 import { run } from './day9'
+import { run as run2 } from './day9-part2'
 const file = Bun.file("../input/day9.txt");
 const input = await file.text();
 
@@ -16,6 +17,9 @@ const testNegativeValues = `
 10 13 16 21 30 45
 `
 
+const part2test = `
+10 13 16 21 30 45
+`
 
 describe("day 9", () => {
 
@@ -29,5 +33,17 @@ describe("day 9", () => {
 
     test("part 1 - input", () => {
         expect(run(input)).toBe(2043183816)
+    });
+
+    test("part 2 - test input 2", () => {
+        expect(run2(part2test)).toBe(5)
+    });
+
+    test("part 2 - test input", () => {
+        expect(run2(testInput)).toBe(2)
+    });
+
+    test("part 2 - input", () => {
+        expect(run2(input)).toBe(1118)
     });
 })
