48bec658b7d05a92d9a05b358d38e1bb829b5b8c
Daniel Stocks <daniel@webcloud.se>
2024-02-14 21:30:10 +0100

    day 4 typescript

---
+++ 2023/typescript/day4-2.ts
@@ -0,0 +1,53 @@
+type cards = number[][][];
+
+function getWinningCards(cards: cards) {
+  return cards.map((card) => {
+    // Filter out winning numbers and calculate score
+    const [winninNumbers, playerNumbers] = card;
+    const matches = playerNumbers.filter(
+      (number: number) => winninNumbers.indexOf(number) !== -1
+    );
+    card[2] = matches;
+    return card;
+  });
+}
+
+export function run(input: string) {
+  const cards = input
+    .trim()
+    .split("\n")
+    .map((card) => {
+      return card
+        .split(/[:\|]+/)
+        .slice(1)
+        .map((numbers) =>
+          numbers
+            .replace(/^\s+|\s+$|\s+(?=\s)/g, "")
+            .split(" ")
+            .map((str) => parseInt(str, 10))
+        );
+    });
+
+  const winningCardsWithNumbers = getWinningCards(cards);
+
+  let x = 0;
+
+  function drill(newCards: cards) {
+    if (newCards.length == 0) {
+      return;
+    }
+    x += newCards.length;
+    newCards.forEach((card, i) => {
+      const index = cards.indexOf(card);
+      const winningNumbers = winningCardsWithNumbers[index][2];
+      if (winningNumbers.length == 0) return;
+      const nextNewCards = cards.slice(
+        index + 1,
+        winningNumbers.length + index + 1
+      );
+      drill(nextNewCards);
+    });
+  }
+  drill(cards);
+  return x;
+}
