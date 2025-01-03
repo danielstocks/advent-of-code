0452242a75ec06b1fc6e257cf8b6e02e403d02cc
Daniel <daniel@webcloud.se>
2025-01-03 23:16:04 +0100

    prettify

--- 2024/ocaml/bin/main.ml
+++ 2024/ocaml/bin/main.ml
@@ -7,7 +7,8 @@ let run ~fn ~day ~part ~mode ~input ~result =
   match filter with
   | Some(filter) when filter <> day -> ()
   | _ -> let (output, time) = Ocaml.Benchmark.time_function fn input in
-    Printf.printf "\nDay %s, part %s, %s result: %d | Benchmark: %.2fms" day part mode output time;
+    let ftime = Printf.sprintf "%.2fms" time in
+    Printf.printf "\nDay %s, part %s, input: %s | Benchmark: %-9s" day part mode ftime;
     match output == result with
       | true -> 
           Printf.printf "%s OK %s" green reset;
@@ -22,9 +23,9 @@ let run ~fn ~day ~part ~mode ~input ~result =
 let day01_input = Ocaml.File.read "../input/day01.txt"
 let day01_test_input = Ocaml.File.read "../input/day01_test.txt"
 let () = run ~day:"01" ~part:"1" ~mode:"test" ~input:day01_test_input ~fn:Ocaml.Day01_1.run ~result:11 
-let () = run ~day:"01" ~part:"1" ~mode:"actual" ~input:day01_input ~fn:Ocaml.Day01_1.run ~result:2166959
+let () = run ~day:"01" ~part:"1" ~mode:"real" ~input:day01_input ~fn:Ocaml.Day01_1.run ~result:2166959
 let () = run ~day:"01" ~part:"2" ~mode:"test" ~input:day01_test_input ~fn:Ocaml.Day01_2.run ~result:31
-let () = run ~day:"01" ~part:"2" ~mode:"actual" ~input:day01_input ~fn:Ocaml.Day01_2.run ~result:23741109
+let () = run ~day:"01" ~part:"2" ~mode:"real" ~input:day01_input ~fn:Ocaml.Day01_2.run ~result:23741109
 
 (* 
    --- DAY TWO ---
@@ -32,9 +33,9 @@ let () = run ~day:"01" ~part:"2" ~mode:"actual" ~input:day01_input ~fn:Ocaml.Day
 let day02_input = Ocaml.File.read "../input/day02.txt"
 let day02_test_input = Ocaml.File.read "../input/day02_test.txt"
 let () = run ~day:"02" ~part:"1" ~mode:"test" ~input:day02_test_input ~fn:Ocaml.Day02_1.run ~result:2
-let () = run ~day:"02" ~part:"1" ~mode:"actual" ~input:day02_input ~fn:Ocaml.Day02_1.run ~result:402
+let () = run ~day:"02" ~part:"1" ~mode:"real" ~input:day02_input ~fn:Ocaml.Day02_1.run ~result:402
 let () = run ~day:"02" ~part:"2" ~mode:"test" ~input:day02_test_input ~fn:Ocaml.Day02_2.run ~result:4
-let () = run ~day:"02" ~part:"2" ~mode:"actual" ~input:day02_input ~fn:Ocaml.Day02_2.run ~result:455
+let () = run ~day:"02" ~part:"2" ~mode:"real" ~input:day02_input ~fn:Ocaml.Day02_2.run ~result:455
 
 (* 
    --- DAY THREE ---
@@ -43,9 +44,9 @@ let day03_input = Ocaml.File.read "../input/day03.txt"
 let day03_test_input = Ocaml.File.read "../input/day03_test.txt"
 let day03_test_input_2 = Ocaml.File.read "../input/day03_test_2.txt"
 let () = run ~day:"03" ~part:"1" ~mode:"test" ~input:day03_test_input ~fn:Ocaml.Day03_1.run ~result:161
-let () = run ~day:"03" ~part:"1" ~mode:"actual" ~input:day03_input ~fn:Ocaml.Day03_1.run ~result:160672468
+let () = run ~day:"03" ~part:"1" ~mode:"real" ~input:day03_input ~fn:Ocaml.Day03_1.run ~result:160672468
 let () = run ~day:"03" ~part:"2" ~mode:"test" ~input:day03_test_input_2 ~fn:Ocaml.Day03_2.run ~result:48
-let () = run ~day:"03" ~part:"2" ~mode:"actual" ~input:day03_input ~fn:Ocaml.Day03_2.run ~result:84893551
+let () = run ~day:"03" ~part:"2" ~mode:"real" ~input:day03_input ~fn:Ocaml.Day03_2.run ~result:84893551
 
 (* 
   --- DAY FOUR ---
@@ -54,9 +55,9 @@ let day04_input = Ocaml.File.read "../input/day04.txt"
 let day04_test_input = Ocaml.File.read "../input/day04_test.txt"
 let day04_test_input_2 = Ocaml.File.read "../input/day04_test_2.txt"
 let () = run ~day:"04" ~part:"1" ~mode:"test" ~input:day04_test_input ~fn:Ocaml.Day04_1.run ~result:18
-let () = run ~day:"04" ~part:"1" ~mode:"actual" ~input:day04_input ~fn:Ocaml.Day04_1.run ~result:2549
+let () = run ~day:"04" ~part:"1" ~mode:"real" ~input:day04_input ~fn:Ocaml.Day04_1.run ~result:2549
 let () = run ~day:"04" ~part:"2" ~mode:"test" ~input:day04_test_input_2 ~fn:Ocaml.Day04_2.run ~result:9
-let () = run ~day:"04" ~part:"2" ~mode:"actual" ~input:day04_input ~fn:Ocaml.Day04_2.run ~result:2003
+let () = run ~day:"04" ~part:"2" ~mode:"real" ~input:day04_input ~fn:Ocaml.Day04_2.run ~result:2003
 
 (* 
   --- DAY FIVE ---
@@ -64,9 +65,9 @@ let () = run ~day:"04" ~part:"2" ~mode:"actual" ~input:day04_input ~fn:Ocaml.Day
 let day05_test_input = Ocaml.File.read "../input/day05_test.txt"
 let day05_input = Ocaml.File.read "../input/day05.txt"
 let () = run ~day:"05" ~part:"1" ~mode:"test" ~input:day05_test_input ~fn:Ocaml.Day05_1.run ~result:143
-let () = run ~day:"05" ~part:"1" ~mode:"actual" ~input:day05_input ~fn:Ocaml.Day05_1.run ~result:5639
+let () = run ~day:"05" ~part:"1" ~mode:"real" ~input:day05_input ~fn:Ocaml.Day05_1.run ~result:5639
 let () = run ~day:"05" ~part:"2" ~mode:"test" ~input:day05_test_input ~fn:Ocaml.Day05_2.run ~result:123
-let () = run ~day:"05" ~part:"2" ~mode:"actual" ~input:day05_input ~fn:Ocaml.Day05_2.run ~result:5273
+let () = run ~day:"05" ~part:"2" ~mode:"real" ~input:day05_input ~fn:Ocaml.Day05_2.run ~result:5273
 
 (* 
   --- DAY SIX ---
@@ -74,9 +75,9 @@ let () = run ~day:"05" ~part:"2" ~mode:"actual" ~input:day05_input ~fn:Ocaml.Day
 let day06_input = Ocaml.File.read "../input/day06.txt" 
 let day06_test_input = Ocaml.File.read "../input/day06_test.txt"
 let () = run ~day:"06" ~part:"1" ~mode:"test" ~input:day06_test_input ~fn:Ocaml.Day06_1.run ~result:41
-let () = run ~day:"06" ~part:"1" ~mode:"actual" ~input:day06_input ~fn:Ocaml.Day06_1.run ~result:5461
+let () = run ~day:"06" ~part:"1" ~mode:"real" ~input:day06_input ~fn:Ocaml.Day06_1.run ~result:5461
 let () = run ~day:"06" ~part:"2" ~mode:"test" ~input:day06_test_input ~fn:Ocaml.Day06_2.run ~result:6
-let () = run ~day:"06" ~part:"2" ~mode:"actual" ~input:day06_input ~fn:Ocaml.Day06_2.run ~result:1836
+let () = run ~day:"06" ~part:"2" ~mode:"real" ~input:day06_input ~fn:Ocaml.Day06_2.run ~result:1836
 
 (* 
   --- DAY SEVEN ---
@@ -84,9 +85,9 @@ let () = run ~day:"06" ~part:"2" ~mode:"actual" ~input:day06_input ~fn:Ocaml.Day
 let day07_test_input = Ocaml.File.read "../input/day07_test.txt"
 let day07_input = Ocaml.File.read "../input/day07.txt"
 let () = run ~day:"07" ~part:"1" ~mode:"test" ~input:day07_test_input ~fn:Ocaml.Day07_1.run ~result:3749
-let () = run ~day:"07" ~part:"1" ~mode:"actual" ~input:day07_input ~fn:Ocaml.Day07_1.run ~result:465126289353
+let () = run ~day:"07" ~part:"1" ~mode:"real" ~input:day07_input ~fn:Ocaml.Day07_1.run ~result:465126289353
 let () = run ~day:"07" ~part:"2" ~mode:"test" ~input:day07_test_input ~fn:Ocaml.Day07_2.run ~result:11387
-let () = run ~day:"07" ~part:"2" ~mode:"actual" ~input:day07_input ~fn:Ocaml.Day07_2.run ~result:70597497486371
+let () = run ~day:"07" ~part:"2" ~mode:"real" ~input:day07_input ~fn:Ocaml.Day07_2.run ~result:70597497486371
 
 (* 
   --- DAY EIGHT ---
@@ -97,10 +98,10 @@ let day08_test_input = Ocaml.File.read "../input/day08_test.txt"
 let day08_input = Ocaml.File.read "../input/day08.txt"
 let () = run ~day:"08" ~part:"1" ~mode:"test" ~input:day08_test_input_2 ~fn:Ocaml.Day08_1.run ~result:4
 let () = run ~day:"08" ~part:"1" ~mode:"test" ~input:day08_test_input ~fn:Ocaml.Day08_1.run ~result:14
-let () = run ~day:"08" ~part:"1" ~mode:"actual" ~input:day08_input ~fn:Ocaml.Day08_1.run ~result:299
+let () = run ~day:"08" ~part:"1" ~mode:"real" ~input:day08_input ~fn:Ocaml.Day08_1.run ~result:299
 let () = run ~day:"08" ~part:"2" ~mode:"test" ~input:day08_test_input ~fn:Ocaml.Day08_2.run ~result:34
 let () = run ~day:"08" ~part:"2" ~mode:"test" ~input:day08_test_input_3 ~fn:Ocaml.Day08_2.run ~result:9
-let () = run ~day:"08" ~part:"2" ~mode:"actual" ~input:day08_input ~fn:Ocaml.Day08_2.run ~result:1032
+let () = run ~day:"08" ~part:"2" ~mode:"real" ~input:day08_input ~fn:Ocaml.Day08_2.run ~result:1032
 
 (* 
   --- DAY NINE ---
@@ -108,7 +109,7 @@ let () = run ~day:"08" ~part:"2" ~mode:"actual" ~input:day08_input ~fn:Ocaml.Day
 let day09_test_input = Ocaml.File.read "../input/day09_test.txt"
 let day09_input = Ocaml.File.read "../input/day09.txt"
 let () = run ~day:"09" ~part:"1" ~mode:"test" ~input:day09_test_input ~fn:Ocaml.Day09_1.run ~result:1928
-let () = run ~day:"09" ~part:"1" ~mode:"actual" ~input:day09_input ~fn:Ocaml.Day09_1.run ~result:6320029754031
+let () = run ~day:"09" ~part:"1" ~mode:"real" ~input:day09_input ~fn:Ocaml.Day09_1.run ~result:6320029754031
 
 (* THE END *)
 let () = print_endline ""
---
+++ 2024/ocaml/lib/day09_2.ml
@@ -0,0 +1,68 @@
+let string_to_int_list string =
+  List.init (String.length string) (fun index ->  
+    let char = String.get string index in
+    if char == '.' then 
+      -1 
+    else 
+      int_of_string (String.make 1 (char))
+  )
+
+let pop list =
+  match list with
+  | [] -> failwith "Can't pop empty list"
+  | x :: xs -> (x, xs)
+
+let diskmap_to_blocks list = list 
+  |> List.mapi(fun index int ->
+      if index mod 2 == 0 then
+        List.init int (fun _ -> index / 2)
+      else
+        List.init int (fun _ -> -1)
+  )
+  |> List.flatten
+
+let defrag_blocks blocks =
+  let array = Array.of_list blocks in
+  let len = Array.length array in
+  let rec find_free idx =
+    if idx >= len then None
+    else if array.(idx) = -1 then Some idx
+    else find_free (idx + 1)
+  in
+  let rec find_used idx =
+    if idx < 0 then None 
+    else if array.(idx) <> -1 then Some idx
+    else find_used (idx - 1)
+  in
+  let rec defrag_step free_idx =
+    match find_free free_idx with
+    | None -> ()
+    | Some free_pos ->
+      match find_used (len - 1) with
+      | None -> ()
+      | Some used_pos ->
+        if free_pos < used_pos then begin
+          array.(free_pos) <- array.(used_pos);
+          array.(used_pos) <- -1;
+          defrag_step (free_pos + 1)
+        end
+  in
+  defrag_step 0;
+  array
+
+let calculate_checksum blocks =
+  blocks 
+    |> Array.fold_left(fun (acc, index) block ->
+      if block > -1 then
+        (acc + block * index, index +1)
+      else
+        (acc, index+1)
+    ) (0, 0) 
+    |> fst
+
+let run data = data
+    |> String.trim 
+    |> string_to_int_list
+    |> diskmap_to_blocks
+    |> defrag_blocks
+    |> calculate_checksum
