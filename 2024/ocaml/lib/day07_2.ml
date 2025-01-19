7ec2518420cab29cdf018ebeed4590fe7635937a
Daniel <daniel@webcloud.se>
2025-01-19 11:13:34 +0100

    cache expressions

--- 2024/ocaml/lib/day07_2.ml
+++ 2024/ocaml/lib/day07_2.ml
@@ -10,23 +10,37 @@ let rec generate_combinations n =
     ]
 
 let find_expressions list = 
-  list |> List.filter_map(fun (sum, numbers) -> 
-    (* Cache this? *)
-    let found_expression = generate_combinations ((List.length numbers) - 1)
-    |> List.exists(fun operators ->
-      sum = List.fold_left2(fun acc operator number ->
-        match operator with 
-        | "*" -> acc * number
-        | "+" -> acc + number
-        | "|" -> int_of_string((string_of_int acc) ^ (string_of_int number))
-        | _ -> failwith "invalid operator"
-      ) (List.hd numbers) operators (List.tl numbers)
+  let cache = Hashtbl.create 26000 in
+  let aux list cache = 
+
+    list |> List.filter_map(fun (sum, numbers) -> 
+      
+      let key = List.length numbers in
+
+      let expressions = match Hashtbl.find_opt cache key with
+      | Some expressions -> expressions
+      | None -> 
+          let result = generate_combinations ((List.length numbers) - 1) in
+          Hashtbl.add cache key result;
+          result 
+      in
+
+      let found_expressions = expressions |> List.exists(fun operators ->
+        sum = List.fold_left2(fun acc operator number ->
+          match operator with 
+          | "*" -> acc * number
+          | "+" -> acc + number
+          | "|" -> int_of_string((string_of_int acc) ^ (string_of_int number))
+          | _ -> failwith "invalid operator"
+        ) (List.hd numbers) operators (List.tl numbers)
+      ) in
+
+      if found_expressions then
+        Some sum
+      else
+        None
     ) in
-    if found_expression then
-      Some sum
-    else
-      None
-  )
+  aux list cache
 
 let run data = data 
   |> String.trim
